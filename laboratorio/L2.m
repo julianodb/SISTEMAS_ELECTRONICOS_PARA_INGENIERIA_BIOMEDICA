cba762c029efd42f59cbb8cd3658275c66c1e0f5
jbarboza <julianodawid@gmail.com>
2023-06-03 09:24:21 -0400

    create real example for L2

--- laboratorio/L2.m
+++ laboratorio/L2.m
@@ -1,12 +1,14 @@
 IS = 1E-12;
 VT = 26e-3;
 
-CH1 = dlmread("DS0001A.CSV", ",", 25, 0);
-CH2 = dlmread("DS0001A.CSV", ",", 25, 0);
+R = 100;
+
+CH1 = dlmread("DS0001C.CSV", ",", 25, 0);
+CH2 = dlmread("DS0001B.CSV", ",", 25, 0);
 
 T = CH1(:,1);
 X = CH1(:,2);
-Y = CH2(:,2).*rand(size(X));
+Y = CH2(:,2)./R;
 close all;
 plot(T, X, ";X;");
 hold on;
@@ -18,7 +20,7 @@ hold on;
 
 figure; hold on;
 for n = 1:0.1:2
-  plot(T, IS*(e.^(T/n/VT)-1), [";n=",num2str(n),";"])
+  plot(X(1:2000), IS*(e.^(X(1:2000)/n/VT)-1), [";n=",num2str(n),";o"])
 endfor
 
 grid on;
