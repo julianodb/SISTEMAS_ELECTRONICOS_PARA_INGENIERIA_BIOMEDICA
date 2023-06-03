d9f146ca0e6038aab58d64cdc24574468fe2d3f3
jbarboza <julianodawid@gmail.com>
2023-06-03 10:43:12 -0400

    add L2_alt.m

---
+++ laboratorio/L2_alt.m
@@ -0,0 +1,10 @@
+pkg load signal
+t = 0:0.0001:0.4;
+ch1 = 2.5*square(t*15*2*pi) + 2.5;
+sys = tf([1],[0.008 1]);
+[ch2, _, _] = lsim(sys, ch1*0.9/5+0.1, t);
+figure;
+hold on;
+plot(t, ch1, ";CH1;")
+plot(t, ch2, ";CH2;")
+grid on;
