585a892a57d9fdbeceb83f26e53a7a5f851f6f96
m3nmql <juliano.barboza@siemens.com>
2024-08-02 15:58:44 -0400

    update 21.m

--- teoria/21.m
+++ teoria/21.m
@@ -2,12 +2,17 @@ pkg load control
 
 s = tf('s');
 
-R1 = 10E3; R2=20E3; C1 = 10E-9; C2=C1; 
+R1 = 10E3; R2=20E3; C1 = 10E-9; C2=C1;
 
-h = 2/(1+2*R1*C1*s+R1*R2*C1*C2*s*s)
+A = 3;
+
+h = A/(1+(4-A)*R1*C1*s+R1*R2*C1*C2*s*s)
 
 t = 0:1e-6:2e-3;
 
+pzmap(h)
+
+figure;
 step(h,t)
 
 figure
@@ -21,3 +26,6 @@ plot(t,y,";w=10k;")
 [y, t] = lsim(h, cos(100_000*t), t);
 
 plot(t,y,"red;w=100k;")
+
+1+2*rand()
+
